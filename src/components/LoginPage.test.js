35385ead5f51e88b24e20e1aa5a6064bdd2a610f
Djenifer R. P <djeniferrenata@yahoo.com.br>
2020-05-30 21:38:42 -0300

    Testa se EmailAndPasswordForm está na página

--- src/components/LoginPage.test.js
+++ src/components/LoginPage.test.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
+import EmailAndPasswordForm from './EmailAndPasswordForm';
 
 test('renders login page', () => {
-  const { container } = render(
+  const div = document.createElement('div');
+  render(
+    <EmailAndPasswordForm buttonLabel="Entrar" />,
+    { container: div }
+  );
+  const emailAndPasswordForm = div.querySelector('form');
+
+  const { container, debug } = render(
     <LoginPage />,
     { wrapper: MemoryRouter }
   );
 
   const loginTitle = container.querySelector('[id="title"]');
+  const loginForm = container.querySelector('form');
 
   const registerLink = container.querySelector('[id="register"]');
   const recoverPasswordLink = container.querySelector('[id="recover-password"]');
 
   expect(loginTitle).toBeInTheDocument();
   expect(loginTitle).toHaveTextContent('Login');
+
+  expect(loginForm).toStrictEqual(emailAndPasswordForm);
 
   expect(registerLink).toBeInTheDocument();
   expect(registerLink).toHaveTextContent('Cadastre-se');
--- src/components/RegisterPage.test.js
+++ src/components/RegisterPage.test.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import RegisterPage from './RegisterPage';
+import EmailAndPasswordForm from './EmailAndPasswordForm';
 
 test('renders register page', () => {
+  const div = document.createElement('div');
+  render(
+    <EmailAndPasswordForm buttonLabel="Cadastrar-se" />,
+    { container: div }
+  );
+  const emailAndPasswordForm = div.querySelector('form');
+
   const { container } = render(
     <RegisterPage />,
     { wrapper: MemoryRouter }
   );
 
   const registerTitle = container.querySelector('[id="title"]');
+  const registerForm = container.querySelector('form');
   const loginLink = container.querySelector('[id="login"]');
 
   expect(registerTitle).toBeInTheDocument();
   expect(registerTitle).toHaveTextContent('Cadastro');
+
+  expect(registerForm).toStrictEqual(emailAndPasswordForm);
 
   expect(loginLink).toBeInTheDocument();
   expect(loginLink).toHaveTextContent('Já possuo conta');
